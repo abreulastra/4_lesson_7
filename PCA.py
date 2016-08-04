3c82220d0a28c8eca97b977d8edbcb39fa654953
Raul Abreu <abreulastra@gmail.com>
2016-08-04 14:59:11 +0300

    LDA and PCA

---
+++ PCA.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  4 14:21:48 2016
+
+@author: AbreuLastra_Work
+"""
+
+import pandas as pd
+
+df = pd.read_csv(
+    filepath_or_buffer='https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',
+    header=None,
+    sep=',')
+
+df.columns=['sepal_len', 'sepal_wid', 'petal_len', 'petal_wid', 'class']
+df.dropna(how="all", inplace=True) # drops the empty line at file-end
+
+df.tail()
+
+# split data table into data X and class labels y
+
+X = df.ix[:,0:4].values
+y = df.ix[:,4].values
+
+from matplotlib import pyplot as plt
+
+
+label_dict = {1: 'Iris-Setosa',
+              2: 'Iris-Versicolor',
+              3: 'Iris-Virgnica'}
+
+feature_dict = {0: 'sepal length [cm]',
+                1: 'sepal width [cm]',
+                2: 'petal length [cm]',
+                3: 'petal width [cm]'}
+
+with plt.style.context('seaborn-whitegrid'):
+    plt.figure(figsize=(8, 6))
+    for cnt in range(4):
+        plt.subplot(2, 2, cnt+1)
+        for lab in ('Iris-setosa', 'Iris-versicolor', 'Iris-virginica'):
+            plt.hist(X[y==lab, cnt],
+                     label=lab,
+                     bins=10,
+                     alpha=0.3,)
+        plt.xlabel(feature_dict[cnt])
+    plt.legend(loc='upper right', fancybox=True, fontsize=8)
+
+    plt.tight_layout()
+    plt.show()
+
+from sklearn.preprocessing import StandardScaler
+X_std = StandardScaler().fit_transform(X)
+
+from sklearn.decomposition import PCA as sklearnPCA
+sklearn_pca = sklearnPCA(n_components=2)
+Y_sklearn = sklearn_pca.fit_transform(X_std)
+
+with plt.style.context('seaborn-whitegrid'):
+    plt.figure(figsize=(6, 4))
+    for lab, col in zip(('Iris-setosa', 'Iris-versicolor', 'Iris-virginica'),
+                        ('blue', 'red', 'green')):
+        plt.scatter(Y_sklearn[y==lab, 0],
+                    Y_sklearn[y==lab, 1],
+                    label=lab,
+                    c=col)
+    plt.xlabel('Principal Component 1')
+    plt.ylabel('Principal Component 2')
+    plt.legend(loc='lower center')
+    plt.tight_layout()
+    plt.show()
